4ab29eef9b05e4f22846d41d2064f33dd6c15cb4
Joseph Holdnak <90223594+jholdnak@users.noreply.github.com>
2025-04-26 14:52:42 -0700

    Animate glass movement

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import createBeer from "./beer";
 
+const targetPositionX: number = -10;
+const otherTargetPositionX: number = 10;
+const endTargetPositionX: number = -20;
+const otherEndTargetPositionX: number = 20;
+var glassesTapped: boolean = false;
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("lightgrey");
 const camera = new THREE.PerspectiveCamera(
@@ -23,8 +29,14 @@ const envTexture = new THREE.CubeTextureLoader()
   .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']);
 scene.environment = envTexture;
 
-const beer = createBeer();
-scene.add(beer);
+const beer_1 = createBeer();
+const beer_2 = createBeer();
+beer_1.position.x = 25;
+beer_2.position.x = -25;
+beer_1.rotation.z = Math.PI / 6;
+beer_2.rotation.set(0, (2 * Math.PI) / 2, Math.PI / 6);
+scene.add(beer_1);
+scene.add(beer_2);
 
 // Lighting setup
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
@@ -38,7 +50,7 @@ const backLight = new THREE.PointLight(0xffffff, 2);
 backLight.position.set(-15, 20, -30);
 scene.add(backLight);
 
-camera.position.set(0, 15, 40);
+camera.position.set(0, 25, 50);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
@@ -46,10 +58,43 @@ function animate() {
   requestAnimationFrame(animate);
   
   // Animate foam
-  beer.children[4].rotation.y += 0.005;
-  beer.children[4].scale.x = 1 + Math.sin(Date.now() * 0.005) * 0.05;
-  beer.children[4].scale.z = beer.children[1].scale.x;
-  
+  beer_1.children[4].rotation.y += 0.005;
+  beer_1.children[4].scale.x = 1 + Math.sin(Date.now() * 0.005) * 0.05;
+  beer_1.children[4].scale.z = beer_1.children[4].scale.x;
+  beer_2.children[4].rotation.y += 0.005;
+  beer_2.children[4].scale.x = 1 + Math.sin(Date.now() * 0.005) * 0.05;
+  beer_2.children[4].scale.z = beer_2.children[4].scale.x;
+
+  // Animate glass movement
+  if (!glassesTapped) {
+    if (beer_1.position.x >= otherTargetPositionX) {
+      // Rotate first glass before the "clink"
+      if (beer_1.rotation.z >= 0)
+        beer_1.rotation.z -= THREE.MathUtils.degToRad(1);
+      beer_1.position.x -= 0.5;
+    }
+    if (beer_2.position.x <= targetPositionX) {
+      // Rotate second glass before the "clink"
+      if (beer_2.rotation.z >= 0)
+        beer_2.rotation.z -= THREE.MathUtils.degToRad(1);
+      beer_2.position.x += 0.5;
+    }
+    if (beer_1.position.x <= otherTargetPositionX || beer_2.position.x >= targetPositionX) {
+      glassesTapped = true;
+    }
+  }
+  else if (glassesTapped) {
+    if (beer_1.position.x <= otherEndTargetPositionX) {
+      if (beer_1.rotation.z != 0)
+        beer_1.rotation.z += Math.PI / 180;
+      beer_1.position.x += 0.5;
+    }
+    if (beer_2.position.x >= endTargetPositionX) {
+      if (beer_2.rotation.z != 0)
+        beer_2.rotation.z += Math.PI / 180;
+      beer_2.position.x -= 0.5;
+    }
+  }
   controls.update();
   renderer.render(scene, camera);
 }
